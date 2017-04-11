22eabae02716ba56a0a7c78b600ee00dfba03344
Adrian Beria <adrberia@hotmail.com>
2017-04-11 13:15:10 -0400

    Create 11_CovertHTMLEntities.js

---
+++ Intermediate Algorithm Scripting/11_CovertHTMLEntities.js
@@ -0,0 +1,14 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+SOLUTION
+*/
+
+function convertHTML(str) {
+  // &colon;&rpar;
+ str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
+return str;
+  
+}
+
+convertHTML("Dolce & Gabbana");
